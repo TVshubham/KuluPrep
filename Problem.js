d43c9c7f8d5dd99dd5406e54d831c24849654d60
TestVagrant <testvagrant@TestVagrants-MacBook-Pro.local>
2024-11-20 14:47:55 +0530

    Added third  testcase

--- Problem.js
+++ Problem.js
@@ -19,11 +19,30 @@ const result1 = filter(arr1, greaterThan10);
 console.log(result1); 
 
  
-function firstIndex(n, i) {
+function firstIndex(n,i) {
     return i === 0;
 }
 const arr2 = [1, 2, 3];
 const result2 = filter(arr2, firstIndex);
 console.log(result2);  
+
+
+function plusOne(n) { return n + 1 }
+const arr3  = [-2,-1,0,1,2]  ;
+
+const result3  = filter(arr3,plusOne)
+
+console.log(result3); 
+
+
+
+
+
+// Input: arr = [-2,-1,0,1,2], fn = function plusOne(n) { return n + 1 }
+// Output: [-2,0,1,2]
+// Explanation:
+// Falsey values such as 0 should be filtered out
+
+// Write 3 separate functions for the 3 examples above.
 
  
